d241aa84a92e9f408b01e8a7a48977bccbfddd4b
Jacobo Fernandez <jacobofb25@gmail.com>
2023-02-25 11:26:51 +0100

    v1

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _servicioUrl: string = "https://api.giphy.com/v1/gifs";
+  private _apiKey: string = "NAgLXjlKxZxfWId63JG6BAm6Jje47z8M";
+  private _historial: string[] = [];
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return[...this._historial];
+  }
+
+  constructor( private http: HttpClient ){
+    
+    if (localStorage.getItem("historial")){
+      this._historial = JSON.parse( localStorage.getItem("historial")! );
+    }
+    this.resultados = JSON.parse( localStorage.getItem("resultados")!) || [] ;
+  }
+
+  buscarGifs( query: string ){
+
+    query = query.trim().toLowerCase();
+
+    if(! this._historial.includes( query )){
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem("historial", JSON.stringify( this._historial ));
+    }
+
+    const params = new HttpParams().set("api_key", this._apiKey).set("limit","12").set("q",query);
+  
+
+    this.http.get<SearchGifsResponse>(`${this._servicioUrl}/search`,{ params }).subscribe(( resp ) =>{
+      this.resultados = resp.data;
+      localStorage.setItem("resultados", JSON.stringify( this.resultados ));
+    });
+  }
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+  get historial(){
+    return this.gifsService.historial;
+  }
+  constructor(private gifsService: GifsService){}
+  
+  buscar( item: string ){
+    this.gifsService.buscarGifs( item );
+  }
+}
